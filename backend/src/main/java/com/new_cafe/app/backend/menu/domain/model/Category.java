1eb286ac0ca3fff8e248508a2e7d30bf2c8cf687
Young <13siban@gmail.com>
2026-02-20 14:31:29 +0900

    jpa 및 로컬db연동

--- backend/src/main/java/com/new_cafe/app/backend/menu/domain/model/Category.java
+++ backend/src/main/java/com/new_cafe/app/backend/menu/domain/model/Category.java
@@ -4,12 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String icon;
--- backend/src/main/java/com/new_cafe/app/backend/menu/domain/model/Menu.java
+++ backend/src/main/java/com/new_cafe/app/backend/menu/domain/model/Menu.java
@@ -6,22 +6,38 @@ import lombok.Data;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Column;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String korName;
     private String engName;
     private String description;
     private Integer price;
+    @Column(name = "category_id")
     private Long categoryId;
     private Boolean isAvailable;
     private Boolean isSoldOut;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
+    @ManyToOne
+    @JoinColumn(name = "category_id", insertable = false, updatable = false)
     private Category category;
+
 }
