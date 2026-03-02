66c03b14725cfde6f4a0d2edefe224b254ee5c76
Young <13siban@gmail.com>
2026-03-03 03:21:42 +0900

    도커 환경 로그인 403 에러 해결 및 세션 쿠키 보안 설정(SESSION_SECURE) 적용

--- backend/src/main/java/com/new_cafe/app/backend/config/security/SecurityBeanConfig.java
+++ backend/src/main/java/com/new_cafe/app/backend/config/security/SecurityBeanConfig.java
@@ -17,12 +17,12 @@ public class SecurityBeanConfig {
         
         // 유저 정보를 가져오는 쿼리 (사용자 ID, 비밀번호, 활성화 여부(enabled) 3개 컬럼 필수)
         manager.setUsersByUsernameQuery(
-            "select nickname username, password, true enabled from users where nickname = ?"
+            "select nickname as username, password, true as enabled from users where nickname = ?"
         );
         
         // 권한(Role)을 가져오는 쿼리 (사용자 ID, 권한명)
         manager.setAuthoritiesByUsernameQuery(
-            "select nickname username, concat('ROLE_', role) authority from users where nickname = ?"
+            "select nickname as username, concat('ROLE_', role) as authority from users where nickname = ?"
         );
         
         return manager;
